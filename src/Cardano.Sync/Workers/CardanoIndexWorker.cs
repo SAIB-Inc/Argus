872f62423bd27a0bc298ec905e8acd49f2885fb6
Clark Alesna <clark_alesna@hotmail.com>
2024-02-07 08:26:36 +0800

    Cleanup CardanoIndexWorker

--- src/Cardano.Sync/Workers/CardanoIndexWorker.cs
+++ src/Cardano.Sync/Workers/CardanoIndexWorker.cs
@@ -20,45 +20,16 @@ public class CardanoIndexWorker<T>(
     IEnumerable<IReducer> reducers
 ) : BackgroundService where T : CardanoDbContext
 {
-    // private readonly NodeClient _nodeClient = new();
-    private readonly IConfiguration _configuration = configuration;
-    private readonly ILogger<CardanoIndexWorker<T>> _logger = logger;
-    private readonly IDbContextFactory<T> _dbContextFactory = dbContextFactory;
-    private readonly IEnumerable<IBlockReducer> _blockReducer = blockReducers;
-    private readonly IEnumerable<ICoreReducer> _coreReducers = coreReducers;
-    private readonly IEnumerable<IReducer> _reducers = reducers;
-    private CardanoDbContext DbContext { get; set; } = null!;
-
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        IList<IReducer> reducers = [.. _blockReducer, .. _coreReducers, .. _reducers];
+        IList<IReducer> combinedReducers = [.. blockReducers, .. coreReducers, .. reducers];
 
-        // ChainSync all reducers in parallel Task.WhenAll
-        await Task.WhenAll(reducers.Select(reducer => ChainSyncReducerAsync(reducer, stoppingToken)));
-
-        // var latestBlock = await DbContext.Blocks.OrderByDescending(b => b.Slot).FirstOrDefaultAsync(cancellationToken: stoppingToken);
-
-        // if (latestBlock is not null)
-        // {
-        //     _configuration["CardanoIndexStartSlot"] = latestBlock.Slot.ToString();
-        //     _configuration["CardanoIndexStartHash"] = latestBlock.Id;
-        // }
-
-        // var tip = await _nodeClient.ConnectAsync(_configuration.GetValue<string>("CardanoNodeSocketPath")!, _configuration.GetValue<ulong>("CardanoNetworkMagic"));
-        // _logger.Log(LogLevel.Information, "Connected to Cardano Node: {Tip}", tip);
-
-        // await _nodeClient.StartChainSyncAsync(new Point(
-        //     _configuration.GetValue<ulong>("CardanoIndexStartSlot"),
-        //     Hash.FromHex(_configuration.GetValue<string>("CardanoIndexStartHash")!)
-        // ));
-
-        // await GetChainSyncResponsesAsync(stoppingToken);
-        // await _nodeClient.DisconnectAsync();
+        await Task.WhenAll(combinedReducers.Select(reducer => ChainSyncReducerAsync(reducer, stoppingToken)));
     }
 
     private async Task ChainSyncReducerAsync(IReducer reducer, CancellationToken stoppingToken)
     {
-        using var dbContext = _dbContextFactory.CreateDbContext();
+        using var dbContext = dbContextFactory.CreateDbContext();
         var nodeClient = new NodeClient();
         var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(stoppingToken);
 
@@ -70,7 +41,7 @@ public class CardanoIndexWorker<T>(
             stopwatch.Start();
 
             var response = e.NextResponse;
-            _logger.Log(
+            logger.Log(
                 LogLevel.Information, "[{reducer}]: New Chain Event {Action}: {Slot} Block: {Block}",
                 CardanoIndexWorker<T>.GetTypeNameWithoutGenerics(reducer.GetType()),
                 response.Action,
@@ -98,7 +69,7 @@ public class CardanoIndexWorker<T>(
 
                                     if (dependencyState == null || dependencyState.Slot < response.Block.Slot)
                                     {
-                                        _logger.Log(LogLevel.Information, "[{Reducer}]: Waiting for dependency {Dependency} Slot {depdencySlot} < {currentSlot}", 
+                                        logger.Log(LogLevel.Information, "[{Reducer}]: Waiting for dependency {Dependency} Slot {depdencySlot} < {currentSlot}", 
                                             GetTypeNameWithoutGenerics(reducer.GetType()), 
                                             dependencyName,
                                             dependencyState?.Slot,
@@ -116,11 +87,11 @@ public class CardanoIndexWorker<T>(
 
                             await reducer.RollForwardAsync(response);
                             reducerStopwatch.Stop();
-                            _logger.Log(LogLevel.Information, "Processed RollForwardAsync[{}] in {ElapsedMilliseconds} ms", GetTypeNameWithoutGenerics(reducer.GetType()), reducerStopwatch.ElapsedMilliseconds);
+                            logger.Log(LogLevel.Information, "Processed RollForwardAsync[{}] in {ElapsedMilliseconds} ms", GetTypeNameWithoutGenerics(reducer.GetType()), reducerStopwatch.ElapsedMilliseconds);
                         }
                         catch(Exception ex)
                         {
-                            _logger.Log(LogLevel.Error, ex, "Error in RollForwardAsync");
+                            logger.Log(LogLevel.Error, ex, "Error in RollForwardAsync");
                             Environment.Exit(1);
                         }
                     }
@@ -134,11 +105,11 @@ public class CardanoIndexWorker<T>(
                             reducerStopwatch.Start();
                             await reducer.RollBackwardAsync(response);
                             reducerStopwatch.Stop();
-                            _logger.Log(LogLevel.Information, "Processed RollBackwardAsync[{}] in {ElapsedMilliseconds} ms", CardanoIndexWorker<T>.GetTypeNameWithoutGenerics(reducer.GetType()), reducerStopwatch.ElapsedMilliseconds);
+                            logger.Log(LogLevel.Information, "Processed RollBackwardAsync[{}] in {ElapsedMilliseconds} ms", CardanoIndexWorker<T>.GetTypeNameWithoutGenerics(reducer.GetType()), reducerStopwatch.ElapsedMilliseconds);
                         }
                         catch(Exception ex)
                         {
-                            _logger.Log(LogLevel.Error, ex, "Error in RollBackwardAsync");
+                            logger.Log(LogLevel.Error, ex, "Error in RollBackwardAsync");
                             Environment.Exit(1);
                         }
                     }
@@ -172,7 +143,7 @@ public class CardanoIndexWorker<T>(
 
             stopwatch.Stop();
 
-            _logger.Log(
+            logger.Log(
                 LogLevel.Information,
                 "[{reducer}]: Processed Chain Event {Action}: {Slot} Block: {Block} in {ElapsedMilliseconds} ms, Mem: {MemoryUsage} MB",
                 CardanoIndexWorker<T>.GetTypeNameWithoutGenerics(reducer.GetType()),
@@ -195,14 +166,14 @@ public class CardanoIndexWorker<T>(
 
         if (latestBlock is not null)
         {
-            _configuration["CardanoIndexStartSlot"] = latestBlock.Slot.ToString();
-            _configuration["CardanoIndexStartHash"] = latestBlock.Id;
+            configuration["CardanoIndexStartSlot"] = latestBlock.Slot.ToString();
+            configuration["CardanoIndexStartHash"] = latestBlock.Id;
         }
 
-        var tip = await nodeClient.ConnectAsync(_configuration.GetValue<string>("CardanoNodeSocketPath")!, _configuration.GetValue<ulong>("CardanoNetworkMagic"));
+        var tip = await nodeClient.ConnectAsync(configuration.GetValue<string>("CardanoNodeSocketPath")!, configuration.GetValue<ulong>("CardanoNetworkMagic"));
         await nodeClient.StartChainSyncAsync(new Point(
-            _configuration.GetValue<ulong>("CardanoIndexStartSlot"),
-            Hash.FromHex(_configuration.GetValue<string>("CardanoIndexStartHash")!)
+            configuration.GetValue<ulong>("CardanoIndexStartSlot"),
+            Hash.FromHex(configuration.GetValue<string>("CardanoIndexStartHash")!)
         ));
 
         try
@@ -229,106 +200,6 @@ public class CardanoIndexWorker<T>(
         }
         return typeName;
     }
-
-
-    // private async Task GetChainSyncResponsesAsync(CancellationToken stoppingToken)
-    // {
-    //     var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(stoppingToken);
-
-    //     void Handler(object? sender, ChainSyncNextResponseEventArgs e)
-    //     {
-    //         if (e.NextResponse.Action == NextResponseAction.Await) return;
-
-    //         var stopwatch = new Stopwatch();
-    //         stopwatch.Start();
-
-    //         var response = e.NextResponse;
-    //         _logger.Log(
-    //             LogLevel.Information, "New Chain Event {Action}: {Slot} Block: {Block}",
-    //             response.Action,
-    //             response.Block.Slot,
-    //             response.Block.Number
-    //         );
-
-    //         var actionMethodMap = new Dictionary<NextResponseAction, Func<IReducer, NextResponse, Task>>
-    //         {
-    //             { NextResponseAction.RollForward, async (reducer, response) =>
-    //                 {
-    //                     try
-    //                     {
-    //                         var reducerStopwatch = new Stopwatch();
-    //                         reducerStopwatch.Start();
-    //                         await reducer.RollForwardAsync(response);
-    //                         reducerStopwatch.Stop();
-    //                         _logger.Log(LogLevel.Information, "Processed RollForwardAsync[{}] in {ElapsedMilliseconds} ms", reducer.GetType(), reducerStopwatch.ElapsedMilliseconds);
-    //                     }
-    //                     catch(Exception ex)
-    //                     {
-    //                         _logger.Log(LogLevel.Error, ex, "Error in RollForwardAsync");
-    //                         Environment.Exit(1);
-    //                     }
-    //                 }
-    //             },
-    //             {
-    //                 NextResponseAction.RollBack, async (reducer, response) =>
-    //                 {
-    //                     try
-    //                     {
-    //                         var reducerStopwatch = new Stopwatch();
-    //                         reducerStopwatch.Start();
-    //                         await reducer.RollBackwardAsync(response);
-    //                         reducerStopwatch.Stop();
-    //                         _logger.Log(LogLevel.Information, "Processed RollBackwardAsync[{}] in {ElapsedMilliseconds} ms", reducer.GetType(), reducerStopwatch.ElapsedMilliseconds);
-    //                     }
-    //                     catch(Exception ex)
-    //                     {
-    //                         _logger.Log(LogLevel.Error, ex, "Error in RollBackwardAsync");
-    //                         Environment.Exit(1);
-    //                     }
-    //                 }
-    //             }
-    //         };
-
-    //         var reducerAction = actionMethodMap[response.Action];
-
-    //         Task.WhenAll(_coreReducers.Select(reducer => reducerAction(reducer, response))).Wait(stoppingToken);
-    //         Task.WhenAll(_reducers.Select(reducer => reducerAction(reducer, response))).Wait(stoppingToken);
-    //         Task.WhenAll(_blockReducer.Select(reducer => reducerAction(reducer, response))).Wait(stoppingToken);
-
-    //         stopwatch.Stop();
-
-    //         _logger.Log(
-    //             LogLevel.Information,
-    //             "Processed Chain Event {Action}: {Slot} Block: {Block} in {ElapsedMilliseconds} ms, Mem: {MemoryUsage} MB",
-    //             response.Action,
-    //             response.Block.Slot,
-    //             response.Block.Number,
-    //             stopwatch.ElapsedMilliseconds,
-    //             Math.Round(GetCurrentMemoryUsageInMB(), 2)
-    //         );
-    //     }
-
-    //     void DisconnectedHandler(object? sender, EventArgs e)
-    //     {
-    //         linkedCts.Cancel();
-    //     }
-
-    //     _nodeClient.ChainSyncNextResponse += Handler;
-    //     _nodeClient.Disconnected += DisconnectedHandler;
-
-    //     try
-    //     {
-    //         while (!stoppingToken.IsCancellationRequested)
-    //         {
-    //             await Task.Delay(100, stoppingToken);
-    //         }
-    //     }
-    //     finally
-    //     {
-    //         _nodeClient.ChainSyncNextResponse -= Handler;
-    //         _nodeClient.Disconnected -= DisconnectedHandler;
-    //     }
-    // }
 
     public static double GetCurrentMemoryUsageInMB()
     {
