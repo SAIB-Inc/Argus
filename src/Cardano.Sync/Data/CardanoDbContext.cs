f0e45a826ec103674986de4dae9a842882bf8acc
Clark Alesna <clark_alesna@hotmail.com>
2024-02-13 14:46:24 +0800

    Add indexes for Block and TransactionOutput entities

--- src/Cardano.Sync/Data/CardanoDbContext.cs
+++ src/Cardano.Sync/Data/CardanoDbContext.cs
@@ -14,7 +14,9 @@ public class CardanoDbContext(DbContextOptions options, IConfiguration configura
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema(_configuration.GetConnectionString("CardanoContextSchema"));
+        modelBuilder.Entity<Block>().HasIndex(b => b.Slot);
         modelBuilder.Entity<Block>().HasKey(b => new { b.Id, b.Number, b.Slot });
+        modelBuilder.Entity<TransactionOutput>().HasIndex(item => item.Slot);
         modelBuilder.Entity<TransactionOutput>().HasKey(item => new { item.Id, item.Index });
         modelBuilder.Entity<TransactionOutput>().OwnsOne(item => item.Amount);
         modelBuilder.Entity<TransactionOutput>().OwnsOne(item => item.Datum);
