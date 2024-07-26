d2e76557ad4de810b3ca993ea9276ec33a536424
Clark Alesna <clark_alesna@hotmail.com>
2024-07-27 04:46:06 +0800

    refactor: improved Tuple cbor converter (#11)

--- src/Cardano.Sync/Data/Models/Datums/SundaeSwap/AssetClass.cs
+++ src/Cardano.Sync/Data/Models/Datums/SundaeSwap/AssetClass.cs
@@ -3,23 +3,6 @@ using CborSerialization;
 
 namespace Cardano.Sync.Data.Models.Datums.SundaeSwap;
 
-[CborSerialize(typeof(AssetClassCborConverter))]
+[CborSerialize(typeof(TupleCborConverter<ByteArray, ByteArray, AssetClass>))]
 public record AssetClass(ByteArray PolicyId, ByteArray AssetName)
     : Tuple<ByteArray, ByteArray>(PolicyId, AssetName);
-
-public class AssetClassCborConverter : ICborConvertor<AssetClass>
-{
-    public AssetClass Read(ref CborReader reader)
-    {
-        var tupleConvert = new TupleCborConverter<ByteArray, ByteArray>();
-        var tuple = tupleConvert.Read(ref reader);
-        return new AssetClass(tuple.First, tuple.Second);
-    }
-
-    public void Write(ref CborWriter writer, AssetClass value)
-    {
-        var tupleConvert = new TupleCborConverter<ByteArray, ByteArray>();
-        var tuple = new Tuple<ByteArray, ByteArray>(value.PolicyId, value.AssetName);
-        tupleConvert.Write(ref writer, tuple);
-    }
-}
