9b29f2e552f953e8e7e251464c37130a2525244b
Clark Alesna <clark_alesna@hotmail.com>
2024-07-26 22:54:10 +0800

    refactor: Introduced CardanoInt datum to support any Int datum types like Lovelace (#7)

--- src/Cardano.Sync/Data/Models/Datums/Lovelace.cs
+++ src/Cardano.Sync/Data/Models/Datums/Lovelace.cs
@@ -1,19 +1,6 @@
-using System.Formats.Cbor;
-using Cardano.Sync.Data.Models.Datums;
 using CborSerialization;
 
-[CborSerialize(typeof(LovelaceCborConvert))]
-public record Lovelace(ulong Value) : IDatum;
+namespace Cardano.Sync.Data.Models.Datums;
 
-public class LovelaceCborConvert: ICborConvertor<Lovelace>
-{
-    public Lovelace Read(ref CborReader reader)
-    {
-        return new Lovelace(reader.ReadUInt64());
-    }
-
-    public void Write(ref CborWriter writer, Lovelace value)
-    {
-        writer.WriteUInt64(value.Value);
-    }
-}
+[CborSerialize(typeof(CardanoIntCborConvert<Lovelace>))]
+public record Lovelace(ulong Value) : CardanoInt(Value);
