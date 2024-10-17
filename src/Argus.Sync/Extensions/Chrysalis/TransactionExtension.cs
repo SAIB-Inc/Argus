a50f5d6313f8751e699112907fa4ef4f3e1e1d63
windss <tamayowendell05@gmail.com>
2024-10-18 05:08:20 +0800

    chore: add utility and transaction extension methods

--- src/Argus.Sync/Extensions/Chrysalis/TransactionExtension.cs
+++ src/Argus.Sync/Extensions/Chrysalis/TransactionExtension.cs
@@ -1,5 +1,6 @@
 using Chrysalis.Cardano.Models.Cbor;
 using Chrysalis.Cardano.Models.Core;
+using Chrysalis.Cardano.Models.Core.Block;
 using Chrysalis.Cardano.Models.Core.Transaction;
 using Chrysalis.Cbor;
 
@@ -61,5 +62,46 @@ public static class TransactionExtension
             CborDefiniteList<ShellyTransactionOutput> list => list.Value,
             CborIndefiniteList<ShellyTransactionOutput> list => list.Value,
             _ => throw new NotImplementedException($"Unsupported output type: {outputs.GetType().Name}")
+        };
+    
+    public static Address TransactionOutputAddress(this TransactionOutput transactionOutput)
+        => transactionOutput switch
+        {
+            BabbageTransactionOutput babbageTransactionOutput => babbageTransactionOutput.Address,
+            AlonzoTransactionOutput alonzoTransactionOutput => alonzoTransactionOutput.Address,
+            MaryTransactionOutput maryTransactionOutput => maryTransactionOutput.Address,
+            ShellyTransactionOutput shellyTransactionOutput => shellyTransactionOutput.Address,
+            _ => throw new NotImplementedException()
+        };
+    
+    public static Value TransactionOutputAmount(this TransactionOutput transactionOutput)
+        => transactionOutput switch
+        {
+            BabbageTransactionOutput babbageTransactionOutput => babbageTransactionOutput.Amount,
+            AlonzoTransactionOutput alonzoTransactionOutput => alonzoTransactionOutput.Amount,
+            MaryTransactionOutput maryTransactionOutput => maryTransactionOutput.Amount,
+            ShellyTransactionOutput shellyTransactionOutput => shellyTransactionOutput.Amount,
+            _ => throw new NotImplementedException()
+        };
+    
+    public static byte[]? TransactionOutputScriptRef(this TransactionOutput transactionOutput)
+        => transactionOutput switch
+        {
+            BabbageTransactionOutput babbageTransactionOutput => babbageTransactionOutput?.ScriptRef?.Value,
+            _ => null
+        };
+    
+    public static DatumOption? TransactionOutputDatumOption(this TransactionOutput transactionOutput)
+        => transactionOutput switch
+        {
+            BabbageTransactionOutput babbageTransactionOutput => babbageTransactionOutput.Datum,
+            _ => null
+        };
+    
+    public static byte[]? TransactionOutputDatumHash(this TransactionOutput transactionOutput)
+        => transactionOutput switch
+        {
+            AlonzoTransactionOutput alonzoTransactionOutput => alonzoTransactionOutput.DatumHash.Value,
+            _ => throw new NotImplementedException()
         };
 }
--- src/Argus.Sync/Utils/ArgusUtils.cs
+++ src/Argus.Sync/Utils/ArgusUtils.cs
@@ -1,3 +1,7 @@
+using CardanoSharp.Wallet.Enums;
+using CardanoSharpAddress = CardanoSharp.Wallet.Models.Addresses.Address;
+using Microsoft.Extensions.Configuration;
+
 namespace Argus.Sync.Utils;
 
 public static class ArgusUtils
@@ -11,5 +15,28 @@ public static class ArgusUtils
             typeName = typeName[..genericCharIndex];
         }
         return typeName;
+    }
+    
+    public static NetworkType GetNetworkType(IConfiguration configuration)
+    {
+        return configuration.GetValue<int>("CardanoNetworkMagic") switch
+        {
+            764824073 => NetworkType.Mainnet,
+            1 => NetworkType.Preprod,
+            2 => NetworkType.Preview,
+            _ => throw new NotImplementedException()
+        };
+    }
+    
+    public static string? ToBech32(this byte[] address)
+    {
+        try
+        {
+            return new CardanoSharpAddress(address).ToString();
+        }
+        catch
+        {
+            return null;
+        }
     }
 }
