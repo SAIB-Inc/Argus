0465fc05359abe9b3c6c9f4b4a89dea45a876121
Clark Alesna <clark_alesna@hotmail.com>
2024-10-20 11:43:13 +0800

    Clean Up

--- src/Argus.Sync/Data/CardanoDbContext.cs
+++ src/Argus.Sync/Data/CardanoDbContext.cs
@@ -14,15 +14,6 @@ public class CardanoDbContext(
 {
     private readonly IConfiguration _configuration = configuration;
     public DbSet<ReducerState> ReducerStates => Set<ReducerState>();
-    public DbSet<TestModel> TestModels => Set<TestModel>();
-    public DbSet<TransactionOutput> TransactionOutputs => Set<TransactionOutput>();
-    public DbSet<SundaeSwapTokenPrice> SundaeSwapTokenPrices => Set<SundaeSwapTokenPrice>();
-    public DbSet<JpegPriceBySubject> JpegPriceBySubjects => Set<JpegPriceBySubject>();
-
-    public DbSet<BalanceByAddress> BalanceByAddress=> Set<BalanceByAddress>();
-    public DbSet<TxBySlot> TxBySlot=> Set<TxBySlot>();
-    public DbSet<BlockBySlot> BlockBySlot=> Set<BlockBySlot>();
-    public DbSet<SplashTokenPrice> SplashTokenPrice => Set<SplashTokenPrice>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -36,46 +27,6 @@ public class CardanoDbContext(
         }
 
         modelBuilder.HasDefaultSchema(_configuration.GetConnectionString("CardanoContextSchema"));
-
-        modelBuilder.Entity<TransactionOutput>(entity =>
-        {
-            entity.HasKey(item => new { item.Id, item.Index });
-
-            entity.HasIndex(item => item.Id);
-            entity.HasIndex(item => item.Index);
-            entity.HasIndex(item => item.Slot);
-            entity.HasIndex(item => item.Address);
-            entity.HasIndex(item => item.UtxoStatus);
-
-            entity.OwnsOne(item => item.Datum);
-            entity.Ignore(item => item.Amount);
-        });
-        
-        modelBuilder.Entity<SundaeSwapTokenPrice>(entity =>
-        {
-            entity.HasKey(item => new { item.TokenXSubject, item.TokenYSubject, item.TxHash, item.TxIndex, item.Slot });
-
-            entity.HasIndex(item => item.TokenXSubject);
-            entity.HasIndex(item => item.TokenYSubject);
-            entity.HasIndex(item => item.Slot);
-            entity.HasIndex(item => item.TxHash);
-        });
-
-        modelBuilder.Entity<JpegPriceBySubject>(entity =>
-        {
-            entity.HasKey(e => new { e.Slot, e.TxHash, e.TxIndex, e.Subject, e.Status });
-
-            entity.HasIndex(e => e.Slot);
-            entity.HasIndex(e => e.Subject);
-            entity.HasIndex(e => e.Status);
-        });
-
-        modelBuilder.Entity<TestModel>().HasKey(x => x.Slot);
-        modelBuilder.Entity<ReducerState>().HasKey(x => x.Name);
-        modelBuilder.Entity<BalanceByAddress>().HasKey(x => x.Address);
-        modelBuilder.Entity<TxBySlot>().HasKey(x => new {x.BlockSlot, x.BlockHash, x.Transaction}); //moerror kung icomment
-        modelBuilder.Entity<BlockBySlot>().HasKey(x => new {x.Slot, x.Hash});
         base.OnModelCreating(modelBuilder);
-        
     }
 }
