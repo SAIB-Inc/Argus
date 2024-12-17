f313795165cd63df6a262fd6471f0c5c06d04871
rjlacanlaled <rjlacanlaled@gmail.com>
2024-12-16 22:12:13 -0500

    Revert "removed chrysalis extensions"
    This reverts commit 7498b86addc9122a6762c107cfb5a1e601829e0d.

---
+++ src/Argus.Sync/Extensions/Chrysalis/TransactionExtension.cs
@@ -0,0 +1,45 @@
+using Chrysalis.Cardano.Core;
+using Chrysalis.Cardano.Core.Types.Block.Transaction.Output;
+
+namespace Argus.Sync.Extensions.Chrysalis;
+
+public static class TransactionExtension
+{
+
+    public static (Address Address, Value Amount) GetComponents(this TransactionOutput output)
+        => output switch
+        {
+            BabbageTransactionOutput x => (x.Address, x.Amount),
+            AlonzoTransactionOutput x => (x.Address, x.Amount),
+            MaryTransactionOutput x => (x.Address, x.Amount),
+            _ => throw new NotImplementedException($"Unsupported TransactionOutput type: {output.GetType().Name}")
+        };
+
+    public static ulong GetCoin(this Value value)
+        => value switch
+        {
+            Lovelace lovelace => lovelace.Value,
+            LovelaceWithMultiAsset lovelaceWithMultiAsset => lovelaceWithMultiAsset.Lovelace.Value,
+            _ => throw new NotImplementedException()
+        };
+
+    public static ulong Lovelace(this Value amount)
+        => amount switch
+        {
+            Lovelace lovelace => lovelace.Value,
+            Value value => value switch
+            {
+                Lovelace lovelace => lovelace.Value,
+                LovelaceWithMultiAsset multiasset => multiasset.Lovelace.Value,
+                _ => throw new NotImplementedException()
+            },
+            _ => throw new NotImplementedException()
+        };
+
+    public static LovelaceWithMultiAsset TransactionValueLovelace(this Value value)
+        => value switch
+        {
+            LovelaceWithMultiAsset lovelaceWithMultiAsset => lovelaceWithMultiAsset,
+            _ => throw new NotImplementedException()
+        };
+}
